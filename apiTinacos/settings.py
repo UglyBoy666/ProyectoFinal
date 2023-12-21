26d66c936bc30f3f124ccc344e8d3c47a63cea46
UglyBoy666 <2018452158@teschi.edu.mx>
2023-12-21 16:20:54 -0600

    deploy

--- apiTinacos/settings.py
+++ apiTinacos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_=1@oc*@a@cm=uye1%7klc^jxi5#2d-uyia5*a)z=1dc7)!)uh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
