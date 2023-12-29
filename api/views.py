dae91204a2c3f93262a97c33a5c5c9adfe03f123
brandon180601 <brandon.silva.180@gmail.com>
2023-12-28 22:31:34 -0600

    login

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class inicio(APIView):
-    template_name="index.html"
+    template_name="authentication-login.html"
     def get(self, request):
         return render(request, self.template_name)
     def post(self, request):
