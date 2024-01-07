ae741734d7c2b5d215a18368130befed47309b1b
brandon180601 <brandon.silva.180@gmail.com>
2024-01-07 09:37:57 -0600

    page_bd_full

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class Estado(models.Model):
     idEstado = models.IntegerField(primary_key=True)
     nombre_estado = models.CharField(max_length=45, unique=True)
